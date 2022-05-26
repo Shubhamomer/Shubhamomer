02ff935548dc882a9208beb26d9804205d543f1b
Shubham Omer <87802106+Shubhamomer@users.noreply.github.com>
2022-05-26 14:56:09 +0530

    Add files via upload

---
+++ Bh.js
@@ -0,0 +1,14 @@
+//var setings
+var name = ("indi website");
+   var a = (20);
+ var b = (30) ;
+ var c = (a+b);
+ 
+ //document setings
+document.write('hello'+name);
+ alert('wellcum to practice program');
+ document.write ("<br>c");
+ 
+function setup(){
+    createcanvas(200,200)
+}
